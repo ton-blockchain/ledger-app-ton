ace007b9560c11a2fe7fac68de24b68cff724aaa
Steve Kite <steve@korshakov.com>
2022-05-31 08:28:00 +0000

    release: v1.1.0

--- tests/ledgercomm/test_name_version.py
+++ tests/ledgercomm/test_name_version.py
@@ -3,5 +3,5 @@ def test_get_app_and_version(cmd, hid):
         # for now it doesn't work with Speculos
         app_name, version = cmd.get_app_and_version()
 
-        assert app_name == "Boilerplate"
-        assert version == "1.0.1"
+        assert app_name == "TON"
+        assert version == "1.1.0"
