5433fe1fa535005c4ec415433c874de2562c207e
Steve Kite <steve@korshakov.com>
2022-10-21 19:24:06 +0400

    fix: improving tests for error codes

--- tests/ledgercomm/boilerplate_client/exception/device_exception.py
+++ tests/ledgercomm/boilerplate_client/exception/device_exception.py
@@ -4,7 +4,6 @@ from typing import Dict, Any, Union
 from .errors import *
 
 
-
 class DeviceException(Exception):  # pylint: disable=too-few-public-methods
     exc: Dict[int, Any] = {
         0x6985: DenyError,
@@ -13,7 +12,6 @@ class DeviceException(Exception):  # pylint: disable=too-few-public-methods
         0x6D00: InsNotSupportedError,
         0x6E00: ClaNotSupportedError,
         0xB000: WrongResponseLengthError,
-        0xB001: DisplayBip32PathFailError,
         0xB002: DisplayAddressFailError,
         0xB003: DisplayAmountFailError,
         0xB004: WrongTxLengthError,
